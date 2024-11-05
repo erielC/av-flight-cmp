bdb1930dc665bf1857a47181f6721cc86c757797
Eriel Cabrera <erielcabrera1234@gmail.com>
2024-11-05 17:51:34 -0500

    added archive file to project to store files that are tested/stored so compiler in src folder does not get mixed up

---
+++ archive/blink.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <SPI.h>
+
+#define LED_PIN 13 // Onboard LED pin for Arduino Mega
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT); // Initialize the LED pin as an output
+}
+
+void loop()
+{
+  digitalWrite(LED_PIN, HIGH); // Turn the LED on
+  delay(1000);                 // Wait for a second
+  digitalWrite(LED_PIN, LOW);  // Turn the LED off
+  delay(1000);                 // Wait for a second
+}
