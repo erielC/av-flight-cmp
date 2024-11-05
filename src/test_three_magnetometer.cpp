9c64f565ca848974a056932e404233fb13dc3e95
Eriel Cabrera <erielcabrera1234@gmail.com>
2024-11-05 17:50:40 -0500

    moved all functions to object class functions now to be called via the magnetometer object and outlined at top of file

--- src/test_three_magnetometer.cpp
+++ src/test_three_magnetometer.cpp
@@ -4,14 +4,19 @@
 #include <Wire.h>
 #include <SparkFun_MMC5983MA_Arduino_Library.h>
 
-//////////////////////// *** FUNCTIONS *** //////////////////////////////////
+//////////////////////// *** OBJECT  FUNCTIONS *** //////////////////////////////////
 
-void initial_magnetometer_temp_check();
-void monitor_magnetometer_temp();
-void verify_magnetometer_connection();
-void get_gauss_measurement();
+/* @erielC
+  magnetometer object:
 
-////////////////////////////////////////////////////////////////////////////
+  magnetometer.initialTempCheck();
+  magnetometer.monitorTemp();
+  magnetometer.verifyConnection(MAGNETOMETER_ADDRESS);
+  magnetometer.getGaussMeasurement();
+  
+*/
+
+////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////// *** VARIABLES *** //////////////////////////////////
 
@@ -27,6 +32,8 @@ const uint32_t measurementInterval = 10000;
 
 // Create an instance of the MMC5983MA class as magnetometer
 SFE_MMC5983MA magnetometer;
+
+// the reason a unsigned 8 bit integer is I2C addresses usually at most 7 bits long (saving memory) @erielC
 const uint8_t MAGNETOMETER_ADDRESS = 0x30; // magnetometers I2C address
 
 void setup()
@@ -52,8 +59,8 @@ void setup()
   magnetometer.softReset();
   Serial.println("MMC5983MA is connected and ready");
 
-  initial_magnetometer_temp_check();
-  verify_magnetometer_connection();
+  magnetometer.initialTempCheck();
+  magnetometer.verifyConnection(MAGNETOMETER_ADDRESS);
 }
 
 void loop()
@@ -65,105 +72,11 @@ void loop()
     lastMeasurementTime = millis();
 
     // Perform measurements
-    monitor_magnetometer_temp();
-    verify_magnetometer_connection();
+    magnetometer.monitorTemp();
+    magnetometer.verifyConnection(MAGNETOMETER_ADDRESS);
   }
 
-  get_gauss_measurement();
+  magnetometer.getGaussMeasurement();
 
   delay(100);
-}
-
-void initial_magnetometer_temp_check()
-{
-  // Gets internal temp from integrated thermometer *reason data type is int is sensor temp range is -75C - 125C*
-  float celsius = magnetometer.getTemperature();
-
-  // Monitoring the internal temperature of the sensor, which can be relevant for temperature compensation in magnetic field measurements
-  Serial.println("Die temperature: ");
-  Serial.print("C: ");
-  Serial.println(celsius);
-}
-
-void monitor_magnetometer_temp()
-{
-  float celsius = magnetometer.getTemperature();
-
-  // Check for close-to-limit temperatures
-  if (celsius < -35 || celsius > 100)
-  {
-    // FIXME add function that can turn off sensor and log all data it has recorded
-    // POTENTIAL SOLUTION: make a function that calls it instead of a return statement ex. void handle_over_temperature();
-  }
-  else if (celsius < -10 || celsius > 90)
-  {
-    Serial.print("Close to limit C: ");
-    Serial.println(celsius);
-  }
-  // Check for nominal operating temperature range
-  else if (celsius >= 10 && celsius <= 60)
-  {
-    Serial.print("Nominal Operating Temperature C: ");
-    Serial.println(celsius);
-  }
-}
-
-void verify_magnetometer_connection()
-{
-  // Checking if the I2C connection for the magnetometer address is correct/working
-  Wire.beginTransmission(MAGNETOMETER_ADDRESS);
-
-  if (Wire.endTransmission() == 0)
-  {
-    Serial.println("Magnetometer connected successfully");
-    // Connection is good; proceed with further operations if necessary
-  }
-  else
-  {
-    Serial.println("Failed to connect to the magnetometer");
-    // Connection failed; handle the error (e.g., retry or enter a safe state)
-  }
-}
-
-void get_gauss_measurement()
-{
-  uint32_t currentX = 0;
-  uint32_t currentY = 0;
-  uint32_t currentZ = 0;
-  double scaledX = 0;
-  double scaledY = 0;
-  double scaledZ = 0;
-
-  currentX = magnetometer.getMeasurementX();
-  currentY = magnetometer.getMeasurementY();
-  currentZ = magnetometer.getMeasurementZ();
-
-  // Uncomment if raw data wanted
-  // Serial.print("X axis raw value: ");
-  // Serial.print(currentX);
-  // Serial.print("\tY axis raw value: ");
-  // Serial.print(currentY);
-  // Serial.print("\tZ axis raw value: ");
-  // Serial.println(currentZ);
-
-  // Converting raw data
-  scaledX = (double)currentX - 131072.0;
-  scaledX /= 131072.0;
-  scaledY = (double)currentY - 131072.0;
-  scaledY /= 131072.0;
-  scaledZ = (double)currentZ - 131072.0;
-  scaledZ /= 131072.0;
-
-  // The magnetometer full scale is +/- 8 Gauss
-  // Multiply the scaled values by 8 to convert to Gauss
-  Serial.print("X axis field (Gauss): ");
-  Serial.print(scaledX * 8, 5); // Print with 5 decimal places
-
-  Serial.print("\tY axis field (Gauss): ");
-  Serial.print(scaledY * 8, 5);
-
-  Serial.print("\tZ axis field (Gauss): ");
-  Serial.println(scaledZ * 8, 5);
-
-  Serial.println();
 }
