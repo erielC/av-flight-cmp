9b72282a83018f348c0f348e416fc57e83e16105
Eriel Cabrera <erielcabrera1234@gmail.com>
2024-11-23 13:13:49 -0500

    corrected errors of detection and simplified code to be done in seperate files

---
+++ src/mainV3.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "SensorManager.h"
+
+RocketState currentState = INIT_AND_SYSTEM_CHECK;
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+
+  // Perform one-time sensor checks
+  if (!InitializeAndCheckSensors())
+  {
+    Serial.println("[ERROR] Initialization failed. Stopping.");
+    while (true)
+      ; // Halt on failure
+  }
+
+  // Transition to IDLE state for liftoff monitoring
+  currentState = IDLE;
+  Serial.println("[INFO] System ready. Awaiting liftoff.");
+}
+
+void loop()
+{
+  switch (currentState)
+  {
+  case IDLE:
+    // if (CheckLiftoffConditions())
+    {
+      currentState = BOOST;
+      Serial.println("[STATE] Transitioning to BOOST phase.");
+    }
+    break;
+
+  default:
+    break;
+  }
+}
